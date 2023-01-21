06aa5ea94e9aeb9890073edfeaaf973862fcc573
Thales Fossalussa <thalesvitor2001@gmail.com>
2023-01-21 18:06:12 -0300

    Criando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { ListagemMoedasComponent } from './components/moedas/listagem-moedas/listagem-moedas.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+  {
+    path: "listagem-moedas",
+    component: ListagemMoedasComponent
+  },
+  {
+    path: "**",
+    redirectTo: "home"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
