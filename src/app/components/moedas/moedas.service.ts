ac74316fe0c8141a4707bc38b79584726df33f5d
Thales Fossalussa <thalesvitor2001@gmail.com>
2023-01-27 19:04:28 -0300

    preparando serviço para realizar outras requisições

--- src/app/components/moedas/moedas.service.ts
+++ src/app/components/moedas/moedas.service.ts
@@ -1,3 +1,5 @@
+import { AppComponent } from './../../app.component';
+import { ListagemMoedas } from './listagem-moedas/listagem-moedas';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -7,10 +9,19 @@ import { Observable } from 'rxjs';
 })
 export class MoedasService {
 
-  private readonly APILatest = 'https://api.exchangerate.host/symbols';
+  private API: string = 'https://api.exchangerate.host/';
   constructor(private http: HttpClient) { }
 
-  ultimaCotacao(): Observable<any> {
-    return this.http.get<any>(this.APILatest);
+  listarMoedas(): Observable<ListagemMoedas> {
+    this.API = 'https://api.exchangerate.host/symbols';
+    return this.http.get<ListagemMoedas>(this.API + 'symbols');
+  }
+
+  converter(from: string, to: string, quantidade?: number): Observable<any> {
+    let converter = `${this.API}/convert?from=${from}&to=${to}`;
+    if (quantidade) {
+      converter += `&amount=${quantidade}`;
+    }
+    return this.http.get<ListagemMoedas>(this.API + converter);
   }
 }
