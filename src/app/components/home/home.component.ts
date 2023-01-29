b734eb1fe6acc2c548252f935cb633d81e9d199f
Thales Fossalussa <thalesvitor2001@gmail.com>
2023-01-29 16:49:49 -0300

    Adicionando a cotação ao do dia na tela inicial

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { ConversorDataHora } from './../../shared/utils/conversor-data-hora';
+import { Component, OnInit } from '@angular/core';
+
+import { MoedaConvertida } from '../moedas/moeda-convertida';
+import { MoedasService } from './../moedas/moedas.service';
+import { Conversao } from '../moedas/conversao';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  cotacaoReal: number = 0;
+  cotacaoDolar: number = 0;
+
+  private conversorDataHora = new ConversorDataHora;
+
+  constructor(private service: MoedasService) { }
+
+  ngOnInit(): void {
+    this.service.converter('BRL', 'USD').subscribe((conversao: Conversao) => {
+      this.cotacaoDolar = conversao.result;
+    })
+
+    this.service.converter('USD', 'BRL').subscribe((conversao: Conversao) => {
+      this.cotacaoReal = conversao.result;
+    })
+  }
 }
